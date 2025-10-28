030ada1eb86ab6fc0e20c07f5ce092848a8fa769
이진모 <lsoul1027@gmail.com>
2025-10-28 14:39:20 +0900

    feat: eslint & husky 커밋 시 자동 검사 추가

--- eslint.config.js
+++ eslint.config.js
@@ -3,22 +3,28 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'eslint.config.js']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    ignores: ['dist', 'node_modules', 'eslint.config.js'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      'plugin:prettier/recommended',
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
     },
   },
 ]);
