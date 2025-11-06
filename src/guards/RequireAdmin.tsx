79b209b74506dd805591a3ae07b51a58fd08ca31
이진모 <lsoul1027@gmail.com>
2025-11-06 14:38:11 +0900

    [FE] fix: conflict 해결

--- src/guards/RequireAdmin.tsx
+++ src/guards/RequireAdmin.tsx
@@ -1,6 +1,7 @@
 // import { Navigate, Outlet } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-// import { useAuthStore } from '@/domains/auth/store/auth.store';
+import { useAuthStore } from '@/domains/auth/store/auth.store';
+import { Navigate } from 'react-router-dom';
 
 export default function RequireAdmin() {
   const { role } = useAuthStore();
--- src/guards/RequireAuth.tsx
+++ src/guards/RequireAuth.tsx
@@ -1,10 +1,9 @@
-// import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
-// import { useAuthStore } from '@/domains/auth/store/auth.store';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuthStore } from '@/domains/auth/store/auth.store';
 
 export default function RequireAuth() {
-  // const { accessToken, initializing } = useAuthStore();
-  // const location = useLocation();
+  const { accessToken } = useAuthStore();
+  const location = useLocation();
 
   if (!accessToken) {
     return <Navigate to="/login" replace state={{ from: location }} />;
